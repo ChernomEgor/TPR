95b4251bb1da969651c7e948a21b6927a256aac9
ChernomEgor <Chernomoreceworker@gmail.com>
2025-11-21 13:45:41 +0500

    Add first comment for begin

---
+++ Lab_1/src/main/java/del_to_two.java
@@ -0,0 +1,13 @@
+import static java.lang.System.out;
+
+public class del_to_two {
+    public static void main(String[] args) {
+        out.println("Что-то");
+
+        int num = 10;
+        int sum = num + 11;
+        out.println(sum);
+        out.println();
+
+    }
+}
